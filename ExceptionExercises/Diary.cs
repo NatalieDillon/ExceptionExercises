ad2780bea8cc20d62110d158f6e2602ba9b0721f
NatalieDillon <112569645+NatalieDillon@users.noreply.github.com>
2022-11-01 16:41:19 +0000

    Add project files.

---
+++ ExceptionExercises/Diary.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionExercises
+{
+    public record DiaryRecord(DateOnly Date, TimeOnly Time, string Location, string Description, int Duration = 60);
+    public class Diary
+    {       
+        public static List<DiaryRecord> ReadDiaryEvents(string fileName)
+        {
+            var diaryEvents = new List<DiaryRecord>();
+            using (var streamReader = new StreamReader(fileName))
+            {
+                streamReader.ReadLine();
+                while (!streamReader.EndOfStream)
+                {
+                    string line = streamReader.ReadLine() ?? string.Empty;
+                    string[] fields = line.Split(',');
+                    DateOnly date = DateOnly.ParseExact(fields[0], "yyyyMMdd");
+                    TimeOnly time = TimeOnly.ParseExact(fields[1], "Hss");
+                    int duration = int.Parse(fields[2]);
+                    var diaryEvent = new DiaryRecord(date, time, fields[3], fields[4], duration);
+                    diaryEvents.Add(diaryEvent);
+                }
+            }
+            return diaryEvents;
+        }
+    }
+}
---
+++ ExceptionExercises/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ExceptionExercises
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CakeDivision();
+
+            
+        }
+
+        public static void ReadDiary()
+        {
+            string fileName = @"Files\DiaryEvents.csv";
+            List<DiaryRecord> diaryRecords = Diary.ReadDiaryEvents(fileName);
+            foreach (var entry in diaryRecords)
+            {
+                Console.WriteLine(entry);
+            }
+        }
+
+        public static void CakeDivision()
+        {
+            int pizzaSlices = 32;
+            Console.WriteLine("Please enter the number of children");
+            int children = Convert.ToInt32(Console.ReadLine());
+            int slices = pizzaSlices / children;
+            Console.WriteLine($"Each child will get {slices} slices");
+        }
+    }
+}
